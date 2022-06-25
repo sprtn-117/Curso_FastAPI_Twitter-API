238555a9873305ce268014f690bd6d1838113e5e
Jose Ramon <jose.117@outlook.com>
2022-06-25 00:14:32 -0500

    tweet_post

--- main.py
+++ main.py
@@ -172,8 +172,35 @@ def home():
     summary="Post a tweet",
     tags=["Tweets"]
     )
-def post():
-    pass
+def post(tweet: Tweet = Body(...)):
+    """
+    # **Post a tweet**
+
+    This path operation post a tweet in the app
+
+        Parameters:
+            Request body parameter
+                -tweet: Tweet
+
+        Return a json with the basic tweet information:
+            -tweet_id: UUID
+            -content: str
+            -created_at: datetime
+            -updated_at: Optional[datetime]
+            -by: User
+    """
+    with open("tweets.json", "r+", encoding="utf-8") as file:
+        result = json.load(file)
+        tweet_dict = tweet.dict()
+        tweet_dict["tweet_id"] = str(tweet_dict["tweet_id"])
+        tweet_dict["created_at"] = str(tweet_dict["created_at"])
+        tweet_dict["updated_at"] = str(tweet_dict["updated_at"])
+        tweet_dict["by"]["user_id"] = str(tweet_dict["by"]["user_id"])
+        tweet_dict["by"]["birth_date"] = str(tweet_dict["by"]["birth_date"])
+        result.append(tweet_dict)
+        file.seek(0)
+        file.write(json.dumps(result))
+        return tweet_dict
 
 
 @app.get(
