cb188beac0c85faf847f5b3a699474e8967ea148
Jose Ramon <jose.117@outlook.com>
2022-06-25 00:28:10 -0500

    home

--- main.py
+++ main.py
@@ -162,7 +162,22 @@ def update_a_user():
     tags=["Tweets"]
     )
 def home():
-    pass
+    """
+    # **Show all tweets**
+
+    This path operation show all tweets in the app
+
+        Parameters: None
+        
+        Return a json list with all tweets in the app:
+            -tweet_id: UUID
+            -content: str
+            -created_at: datetime
+            -updated_at: Optional[datetime]
+            -by: User
+    """
+    with open("tweets.json", "r", encoding="utf-8") as file:
+        return json.load(file)
 
 
 @app.post(
